a970077de2c8f18b9feeba304bf3826d61718117
shahimsha786 <121596739+shahimsha786@users.noreply.github.com>
2023-01-24 11:05:28 +0530

    my fisrt commit for jenksins

---
+++ Multiplication2.java
@@ -0,0 +1,15 @@
+package com.add;
+
+public class Multiplication2 {
+
+	public int add2(int x, int y) {
+		return x*y;
+	}
+	public static void main(String[] args) {
+		Multiplication2 add2=new Multiplication2();
+		int z=add2.add2(4, 9);
+		System.out.println("addition of two numbers is  "+z);
+
+	}
+
+}
