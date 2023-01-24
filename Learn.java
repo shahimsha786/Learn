209a35d7cad642882874bf74ef00184ddf08e622
shahimsha786 <121596739+shahimsha786@users.noreply.github.com>
2023-01-24 11:18:54 +0530

    ss

--- Learn.java
+++ Learn.java
@@ -1,12 +1,12 @@
 package com.add;
 
-public class Multiplication2 {
+public class Learn {
 
 	public int add2(int x, int y) {
 		return x*y;
 	}
 	public static void main(String[] args) {
-		Multiplication2 add2=new Multiplication2();
+		Learn add2=new Learn();
 		int z=add2.add2(4, 9);
 		System.out.println("addition of two numbers is  "+z);
 
